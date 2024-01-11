8042014225db332f9767bc1a2699fe82dd7ac58d
daiumahsanbutt <32085886+daiumahsanbutt@users.noreply.github.com>
2024-01-11 11:51:39 +0500

    PandaGo Fixes

--- src/PandaGo/PandaGoClient.php
+++ src/PandaGo/PandaGoClient.php
@@ -115,7 +115,7 @@ class PandaGoClient
         } else {
             return array(
                 "status" => 0,
-                "response" => $response
+                "response" => $response['message']
             );
         }
     }
